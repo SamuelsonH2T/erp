6c1ebcfc003842f602afb06f81334aed386964fd
SamuelsonH2T <samu.elsonh2t@gmail.com>
2017-01-25 23:26:23 -0200

    adcionando utils.models Estado, Cidade

--- apps/utils/admin.py
+++ apps/utils/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from . models import Regiao
+from . models import Regiao, Estado, Cidade
 
 admin.site.register(Regiao)
+admin.site.register(Estado)
+admin.site.register(Cidade)
--- apps/utils/models.py
+++ apps/utils/models.py
@@ -11,3 +11,30 @@ class Regiao(models.Model):
     class Meta:
         verbose_name = 'Regiao'
         verbose_name_plural = 'Regiões'
+
+
+
+class Estado(models.Model):
+    nome = models.CharField(max_length=42, unique=True)
+    sigla = models.CharField(max_length=2, unique=True)
+    regiao = models.ForeignKey(Regiao)
+
+    def __str__(self):
+        return "{n}".format(n=self.nome)
+
+    class Meta:
+        verbose_name = 'Estado'
+        verbose_name_plural = 'Estados'
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=42,unique=True)
+    estado = models.ForeignKey(Estado)
+    ddd = models.CharField(max_length=2)
+
+    def __str__(self):
+        return "{n}".format(n=self.nome)
+
+
+    class Meta:
+        verbose_name = 'Cidade'
+        verbose_name_plural = 'Cidades'
